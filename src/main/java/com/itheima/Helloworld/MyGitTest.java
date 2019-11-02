83ea3bbaf0ed33943ca011971a2eb5e298a77885
chongge <1240636863@qq.com>
2019-11-02 15:41:40 +0800

    提交本地仓库

---
+++ src/main/java/com/itheima/Helloworld/MyGitTest.java
@@ -0,0 +1,12 @@
+package com.itheima.Helloworld;
+
+/**
+ * Author chongge
+ * Time 2019/11/2 15:30
+ * ClassName com.itheima.Helloworld
+ */
+public class MyGitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
