9792bb6031108ffc076d2e2e2c049286c11a507a
chongge <1240636863@qq.com>
2019-11-02 17:41:27 +0800

    提交本地仓库

---
+++ src/main/java/com/itheima/Helloworld/testdev.java
@@ -0,0 +1,12 @@
+package com.itheima.Helloworld;
+
+/**
+ * Author chongge
+ * Time 2019/11/2 17:40
+ * ClassName com.itheima.Helloworld
+ */
+public class testdev {
+    public static void main(String[] args) {
+        System.out.println("test  dev");
+    }
+}
